1537710f7976a81142fa62a57fcf8567d6d3a3a6
Chase Wood <chase@Chases-MacBook-Pro.local>
2020-10-20 15:11:01 -0500

    got server running locally

---
+++ server.js
@@ -0,0 +1,37 @@
+//ENVIRONMENTAL VARIABLES//
+require('dotenv').config();
+const { PORT = 3000, NODE_ENV = 'development' } = process.env;
+
+// MONGO CONNECTION
+const mongoose = require('./db/connection');
+
+// CORS
+const cors = require('cors');
+const corsOptions = require('./configs/cors.js');
+
+// EXPRESS
+const express = require('express');
+const app = express();
+
+// OTHER IMPORTS
+const morgan = require('morgan');
+
+// ROUTER IMPORTS
+// like const dogRouter = require("./controllers/dog")
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+app.use(morgan('tiny'));
+
+// ROUTER FOR TESTING SERVER IS WORKING
+app.get('/', (req, res) => {
+	res.json({ msg: 'Test port, nothing to see here' });
+});
+
+// STATE ROUTES
+
+// LISTENER
+app.listen(PORT, () => {
+	console.log(`You are listening on port ${PORT}`);
+});
